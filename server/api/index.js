f6e708d1797222619ed6c686d28b8142db4b37dd
Marvin9 <marvinduff97@gmail.com>
2020-09-23 17:59:47 +0530

    chore: Add comments to API

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,13 @@
 const APIs = {
+  // get all posts [20 max], add query param named 'page'
   posts: require('./posts'),
+  // get all available categories
   categories: require('./categories'),
+  // get single post
   'posts/:postId': require('./post'),
+  // get 3 related posts
   'posts/:postId/related': require('./related'),
+  // get top 10 tags based on post count
   tags: require('./tags'),
 };
 
